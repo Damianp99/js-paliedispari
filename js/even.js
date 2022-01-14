b5008d68ab099356daaa600ffa8d4c4a545a1841
Damianp99 <damianperez200199@gmail.com>
2022-01-14 16:26:07 +0100

    function pc number done

--- js/even.js
+++ js/even.js
@@ -9,3 +9,31 @@ BONUS:
 Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 */
 
+/*
+    1° Con un prompt facciamo inserire un numero all'utente (1-5)
+    2° Con una funzione generiamo un numero per il PC (1-5)
+    3° Somma dei numeri 
+    4° Stabiliamo se la somma dei numeri scelti da user e PC è pari o dispari
+    5° Dichiara il vincitore
+*/
+
+
+const userNumber = parseInt(prompt('Inserisci un numero'));
+console.log(userNumber)
+
+let pcNumber = pcRandomNum();
+
+function pcRandomNum() {
+    let pcNumber = Math.floor(Math.random() * 5 + 1);
+    console.log(pcNumber)
+}
+
+
+
+
+
+
+
+
+
+
